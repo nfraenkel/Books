65c521bec0e6623ee8df000905e099d3e013b1b0
Nathan Fraenkel <nfraenkel18@gmail.com>
2012-04-21 19:55:47 -0400

    put android stuff into 1 folder, tbAndroid

---
+++ tbAndroid/assets/www/functions.js
@@ -0,0 +1,21 @@
+function loadXMLDoc()
+	{
+		var xmlhttp;
+		if (window.XMLHttpRequest)
+  		{// code for IE7+, Firefox, Chrome, Opera, Safari
+  			xmlhttp=new XMLHttpRequest();
+  		}
+		else
+  		{// code for IE6, IE5
+  			xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  		}
+		xmlhttp.onreadystatechange=function()
+  		{
+  			if (xmlhttp.readyState==4 && xmlhttp.status==200)
+    		{
+    		document.getElementById("myDiv").innerHTML=xmlhttp.responseText;
+    		}
+  		}
+		xmlhttp.open("GET","https://na1.salesforce.com/services/data/",true);
+		xmlhttp.send();
+	}
